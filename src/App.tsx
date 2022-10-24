3f1f2d7b374a08e56e5001c9e4b23619886a3340
Zee530 <Zeee530@gmail.com>
2022-10-24 15:42:20 +0100

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <ChakraProvider>
       <Navbar />
+      <Home />
     </ChakraProvider>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,24 +5,23 @@ import {
   ListIcon,
   OrderedList,
   UnorderedList,
+  Button
 } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
-import logo from './img/nav_logo.png'
+import logo from './img/hat_logo.png'
 
 function Navbar() {
   return (
     <Box bg='#066204' w='100%' color='white' p={4} display='flex'>
-      <Box w='50%'>
-        <Image src={logo} alt='logo_1' ml={12}/>
+      <Box w='80%' display='flex'>
+        <Image src={logo} alt='logo_1' ml={12} width={8} height={8}/>
+        <Box pl={5} fontSize={20} fontWeight='bolder'>PROMOTOTION EXAM PREPPER</Box>
       </Box>
-      <Box w='50%'>
+      <Box w='20%'>
         <List display='flex' justifyContent='space-evenly'>
-          <ListItem>Home</ListItem>
-          <ListItem>About Us</ListItem>
-          <ListItem>Resources</ListItem>
-          <ListItem>Media Center</ListItem>
-          <ListItem>Contact Us</ListItem>
-          <ListItem>FOI Portal</ListItem>
+          <ListItem><Button bg='inherit'>Home</Button></ListItem>
+          <ListItem><Button bg='inherit'>About</Button></ListItem>
+          <ListItem><Button bg='inherit'>GitHub</Button></ListItem>
         </List>
       </Box>
     </Box>
